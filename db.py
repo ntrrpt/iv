5824e4111814b71e75294010910e8b67def450e8
nt <>
2025-08-20 20:42:33 +0300

    id => seq

--- db.py
+++ db.py
@@ -8,53 +8,53 @@ from pathlib import Path
 def init(db: str):
     schema = """
         CREATE TABLE IF NOT EXISTS boards (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            seq INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL UNIQUE,
             description TEXT
         );
 
         CREATE TABLE IF NOT EXISTS threads (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            seq INTEGER PRIMARY KEY AUTOINCREMENT,
             board_id INTEGER NOT NULL,
             title TEXT,
-            FOREIGN KEY(board_id) REFERENCES boards(id)
+            FOREIGN KEY(board_id) REFERENCES boards(seq)
         );
 
         CREATE TABLE IF NOT EXISTS posts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            seq INTEGER PRIMARY KEY AUTOINCREMENT,
             thread_id INTEGER NOT NULL,
             post_id INTEGER NOT NULL UNIQUE,
             author TEXT,
             text TEXT NOT NULL,
             time INTEGER,
-            FOREIGN KEY(thread_id) REFERENCES threads(id)
+            FOREIGN KEY(thread_id) REFERENCES threads(seq)
         );
 
         CREATE TABLE IF NOT EXISTS attachments (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            seq INTEGER PRIMARY KEY AUTOINCREMENT,
             post_id INTEGER NOT NULL,
             file_type TEXT,
             file_name TEXT,
             file_url TEXT,
             thumb_url TEXT,
             file_data BLOB,
-            FOREIGN KEY(post_id) REFERENCES posts(post_id)
+            FOREIGN KEY(post_id) REFERENCES posts(seq)
         );
 
         CREATE VIRTUAL TABLE IF NOT EXISTS posts_fts USING fts5 (
-            text, content='posts', content_rowid='id'
+            text, content='posts', content_rowid='seq'
         );
 
         CREATE TRIGGER IF NOT EXISTS posts_ai AFTER INSERT ON posts BEGIN
-            INSERT INTO posts_fts(rowid, text) VALUES (new.id, new.text);
+            INSERT INTO posts_fts(rowid, text) VALUES (new.seq, new.text);
         END;
 
         CREATE TRIGGER IF NOT EXISTS posts_au AFTER UPDATE ON posts BEGIN
-            UPDATE posts_fts SET text = new.text WHERE rowid = new.id;
+            UPDATE posts_fts SET text = new.text WHERE rowid = new.seq;
         END;
 
         CREATE TRIGGER IF NOT EXISTS posts_ad AFTER DELETE ON posts BEGIN
-            DELETE FROM posts_fts WHERE rowid = old.id;
+            DELETE FROM posts_fts WHERE rowid = old.seq;
         END;
     """
 
@@ -88,7 +88,7 @@ def add_post(db, thread_id, post, path=''):
     with sqlite3.connect(db) as conn:
         cur = conn.cursor()
 
-        q = "SELECT id FROM posts WHERE post_id = ?"
+        q = "SELECT seq FROM posts WHERE post_id = ?"
         cur.execute(q, (post['id'],))
 
         if not cur.fetchone():
@@ -116,20 +116,20 @@ def add_post(db, thread_id, post, path=''):
                     INSERT INTO attachments (post_id, file_type, file_name, file_url, thumb_url, file_data)
                     VALUES (?, ?, ?, ?, ?, ?)
                 """
-                cur.execute(q, (post.get('id'), file_type, file_name, file['url'], file['thumb'], file_data))
+                cur.execute(q, (post['id'], file_type, file_name, file['url'], file['thumb'], file_data))
 
             q = """
                 INSERT INTO posts (thread_id, post_id, author, text, time)
                 VALUES (?, ?, ?, ?, ?)
             """
-            cur.execute(q, (thread_id, post['id'], post.get('author'), post.get('text'), post.get('time')))
+            cur.execute(q, (thread_id, post['id'], post['author'], post['text'], post['time']))
 
 def find_board_by_name(db, name):
     board_id = 0
     with sqlite3.connect(db) as conn:
         cur = conn.cursor()
 
-        q = "SELECT id FROM boards WHERE name = ?"
+        q = "SELECT seq FROM boards WHERE name = ?"
         cur.execute(q, (name,))
         r = cur.fetchone()
         
@@ -137,16 +137,16 @@ def find_board_by_name(db, name):
             board_id = r[0]
     return board_id
 
-def find_thread_by_id(db, thread_id):
+def find_thread_by_seq(db, thread_seq):
     with sqlite3.connect(db) as conn:
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        cur.execute("SELECT id, title FROM threads WHERE id = ?", (thread_id,))
+        cur.execute("SELECT seq, title FROM threads WHERE seq = ?", (thread_seq,))
         thread = dict(cur.fetchone())
 
         q = """
             SELECT
-                p.id AS id,
+                p.seq AS seq,
                 p.post_id AS post_id,
                 p.author,
                 p.text,
@@ -160,19 +160,19 @@ def find_thread_by_id(db, thread_id):
             FROM 
                 posts p
                 LEFT JOIN attachments a ON a.post_id = p.post_id
-                JOIN threads t ON p.thread_id = t.id
-                JOIN boards b ON t.board_id = b.id
+                JOIN threads t ON p.thread_id = t.seq
+                JOIN boards b ON t.board_id = b.seq
             WHERE 
                 p.thread_id = ?
-                ORDER BY p.id ASC;
+                ORDER BY p.seq ASC;
         """
 
-        cur.execute(q, (thread_id,))
+        cur.execute(q, (thread_seq,))
         
         posts = {}
         for row in cur.fetchall():
             r = dict(row)
-            pid = r.get('id')
+            pid = r.get('seq')
             if pid:
                 posts[pid] = {
                     "author": r["author"],
@@ -215,7 +215,7 @@ def find_thread_by_post(db, post_id):
             thread_id = r[0]
 
     if thread_id:
-        return find_thread_by_id(db, thread_id)
+        return find_thread_by_seq(db, thread_id)
 
 def find_posts_by_text(DB, TEXT, LIMIT=50, OFFSET=0, FTS=True):
     with sqlite3.connect(DB) as conn:
@@ -243,7 +243,7 @@ def find_posts_by_text(DB, TEXT, LIMIT=50, OFFSET=0, FTS=True):
 
             q = f"""
                 SELECT
-                    p.id AS id,
+                    p.seq AS seq,
                     p.post_id AS post_id,
                     p.author,
                     p.text,
@@ -257,11 +257,11 @@ def find_posts_by_text(DB, TEXT, LIMIT=50, OFFSET=0, FTS=True):
                 FROM 
                     posts AS p
                     LEFT JOIN attachments AS a ON a.post_id = p.post_id
-                    JOIN threads AS t ON p.thread_id = t.id
-                    JOIN boards AS b ON t.board_id = b.id
+                    JOIN threads AS t ON p.thread_id = t.seq
+                    JOIN boards AS b ON t.board_id = b.seq
                 WHERE 
                     p.text LIKE ? COLLATE NOCASE
-                    ORDER BY p.id ASC
+                    ORDER BY p.seq ASC
                     LIMIT ? OFFSET ?;
             """
 
@@ -272,7 +272,7 @@ def find_posts_by_text(DB, TEXT, LIMIT=50, OFFSET=0, FTS=True):
                     COUNT(*) AS total_count
                 FROM 
                     posts_fts
-                    JOIN posts p ON posts_fts.rowid = p.id
+                    JOIN posts p ON posts_fts.rowid = p.seq
                 WHERE 
                     posts_fts MATCH ?
             """
@@ -283,7 +283,7 @@ def find_posts_by_text(DB, TEXT, LIMIT=50, OFFSET=0, FTS=True):
 
             q = """
                 SELECT
-                    p.id,
+                    p.seq,
                     p.post_id,
                     p.author,
                     p.text,
@@ -296,13 +296,13 @@ def find_posts_by_text(DB, TEXT, LIMIT=50, OFFSET=0, FTS=True):
                     b.name AS board
                 FROM 
                     posts_fts
-                    JOIN posts p ON posts_fts.rowid = p.id
+                    JOIN posts p ON posts_fts.rowid = p.seq
                     LEFT JOIN attachments a ON a.post_id = p.post_id
-                    JOIN threads t ON p.thread_id = t.id
-                    JOIN boards b ON t.board_id = b.id
+                    JOIN threads t ON p.thread_id = t.seq
+                    JOIN boards b ON t.board_id = b.seq
                 WHERE 
                     posts_fts MATCH ?
-                    ORDER BY p.id ASC
+                    ORDER BY p.seq ASC
                     LIMIT ? OFFSET ?;
             """
             cur.execute(q, (TEXT, LIMIT, OFFSET))
@@ -312,7 +312,7 @@ def find_posts_by_text(DB, TEXT, LIMIT=50, OFFSET=0, FTS=True):
         posts = {}
         for row in cur.fetchall():
             r = dict(row)
-            pid = r.get('id')
+            pid = r.get('seq')
             if pid:
                 posts[pid] = {
                     "author": r["author"],
